649dfeb261bda206a47917ed2362be9c80b55540
SoldierCorp <soldiercrp@gmail.com>
2013-04-09 19:29:58 -0500

    New array function to push after to

--- helpers/array.php
+++ helpers/array.php
@@ -31,3 +31,16 @@ if (!function_exists("string2Array")) {
 		return false;
 	}
 }
+
+if(!function_exists("array_push_after")) {
+	function array_push_after($src,$in,$pos)
+	{
+	    if(is_int($pos)) $R=array_merge(array_slice($src,0,$pos+1), $in, array_slice($src,$pos+1));
+	    else{
+	        foreach($src as $k=>$v){
+	            $R[$k]=$v;
+	            if($k==$pos)$R=array_merge($R,$in);
+	        }
+	    }return $R;
+	}
+}

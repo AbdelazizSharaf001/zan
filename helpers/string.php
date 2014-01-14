601531a632165a782e1330fadf7eb4f73fe11707
Carlos Santana Roldán <carlos@milkzoft.com>
2014-01-14 11:11:30 -0800

    Update string.php

--- helpers/string.php
+++ helpers/string.php
@@ -500,7 +500,6 @@ if (!function_exists("showContent")) {
 		$content = removeRareChars($content);
 		$content = str_replace("{{CDN_SERVER}}", getCDN(), $content);
 		$content = str_replace("<p>&nbsp;</p>", "", $content);
-		$content = checkSpelling($content);
 
 		return setCode($content);		
 	}

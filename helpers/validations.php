e91dad7aa05e652ae3de4744a3d60ceb633a69e7
Carlos Santana <carlos@milkzoft.com>
2014-02-12 23:42:46 -0800

    Validations

--- helpers/validations.php
+++ helpers/validations.php
@@ -79,7 +79,7 @@ if (!function_exists("isSPAM")) {
 	function isSPAM($string, $max = 0)
 	{
 		$words = array(	
-			"www", ".co.uk", ".jp", ".ch", ".info", ".mobi", ".us", ".ca", ".ws", ".ag", 
+			"www", ".co.uk", ".jp", ".ch", ".info", ".mobi", ".us", ".ca", ".ws", ".ag", ".pl",
 			".com.co", ".net.co", ".com.ag", ".net.ag", ".it", ".fr", ".tv", ".am", ".asia", ".at", ".be", ".cc", ".de", ".es", ".com.es", ".eu", 
 			".fm", ".in", ".tk", ".com.mx", ".nl", ".nu", ".tw", ".vg", "sex", "porn", "fuck", "buy", "free", "dating", "viagra", "money", "dollars", 
 			"payment", "website", "games", "toys", "poker", "cheap", "[url]", "[/url]", "[url="
@@ -93,7 +93,7 @@ if (!function_exists("isSPAM")) {
 				$count += substr_count($string, $word);
 			}
 		}
-		
+
 		return ($count > $max) ? true : false;
 	}
 }

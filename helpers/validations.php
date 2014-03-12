8cf043d47af6bf60bd751f7d8284e97d81310972
Carlos Santana <carlos@milkzoft.com>
2014-03-12 00:05:48 -0700

    Validations fixed

--- helpers/validations.php
+++ helpers/validations.php
@@ -80,7 +80,7 @@ if (!function_exists("isSPAM")) {
 	function isSPAM($string, $max = 1)
 	{
 		$words = array(	
-			".com", ".net", ".org", ".html", "www", ".co.uk", ".jp", ".ch", ".info", ".mobi", ".us", ".ca", ".ws", ".ag", ".pl", "?????", "href",
+			".edu", ".com", ".net", ".org", ".html", "www", ".co.uk", ".jp", ".ch", ".info", ".mobi", ".us", ".ca", ".ws", ".ag", ".pl", "?????", "href",
 			".com.co", ".net.co", ".com.ag", ".net.ag", ".it", ".fr", ".tv", ".am", ".asia", ".at", ".be", ".cc", ".de", ".es", ".com.es", ".eu", 
 			".fm", ".in", ".tk", ".com.mx", ".nl", ".nu", ".tw", ".vg", "sex", "porn", "fuck", "buy", "free", "dating", "viagra", "money", "dollars", 
 			"payment", "website", "games", "toys", "poker", "cheap", "[url]", "[/url]", "[url="
